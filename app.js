e848349102df19e706e0ea6078cf4e4e80f88ce6
Sikandar Iqbal <sikandar@Sikandars-MacBook-Pro.local>
2020-06-09 15:03:09 +0500

    nice

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const app = require('express')()
 
+
+// Constants
+const PORT = 3000;
+const HOST = "0.0.0.0";
+
 app.get('/', (req, res) => {
   res.send("Hello from Appsody!");
 });
  
 module.exports.app = app;
+
+
+app.listen(PORT, HOST);
+console.log(`Running on ${PORT}`);
